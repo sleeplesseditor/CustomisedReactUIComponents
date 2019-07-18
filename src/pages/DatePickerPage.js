2fa1dc1b76e4382e78ab8899bcb84fa4f4fede12
Josh Bryson <josh@bryson.me.uk>
2019-07-18 14:59:30 +0100

    Initial custom boilerplate setup

---
+++ src/pages/DatePickerPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function DatePickerPage() {
+
+  return (
+    <React.Fragment>
+        <div className="App">
+            <h2>React DatePicker</h2>
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default DatePickerPage;
