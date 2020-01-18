d9941be064c43640efdb544ebc88be43fd5e5090
Josh Bryson <josh@bryson.me.uk>
2020-01-18 18:39:00 +0000

    CollapsibleContent component updated to dynamic method

--- src/components/CollapsibleContent/CollapsibleContent.js
+++ src/components/CollapsibleContent/CollapsibleContent.js
@@ -3,18 +3,42 @@ import './CollapsibleContent.scss';
 
 class CollapsibleContent extends Component {
     constructor(props) {
-      super(props)
-      this.state = {
-          active: false,
-      }
-      
-      this.getWrapperStyle = this.getWrapperStyle.bind(this);
-      this.toggle = this.toggle.bind(this);
+        super(props)
+        this.tab = React.createRef();
+        this.state = {
+            active: false,
+            content: []
+        }
+        
+        this.setTab = this.setTab.bind(this);
+        this.loadData = this.loadData.bind(this);
+        this.getWrapperStyle = this.getWrapperStyle.bind(this);
+        this.toggle = this.toggle.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadData();
     }
     
+    setTab(ele) {
+        this.tab = ele;
+    }
+
+    loadData() {
+        setTimeout(() => {
+        const elements = [];
+        for(let i =0; i < Math.random()*30; i++) {
+            elements.push(<div key={i} className="collapsible-container__element">Hidden Element</div>);
+        }
+        this.setState({ content: elements })
+        console.log(this.state);
+        }, 1000);
+    }
     
+
     getWrapperStyle() {
-        return this.state.active ? { height: '200px'} : { height: 0 };
+        console.log(this.state, this.tab)
+  	    return this.state.active && this.tab ? { height: this.tab.current.scrollHeight + 10} : { height: 0 };
     }
     
     toggle() {
@@ -25,18 +49,24 @@ class CollapsibleContent extends Component {
     render() {
       return (
             <div className="collapsible-container">
-                <button 
-                    onClick={this.toggle}
-                    className="collapsible-container__button"
-                >
-                    {this.state.active ? 'See Less': 'See More'}
-                </button>
+                <div className="collapsible-container__button-area">
+                    <button 
+                        onClick={this.toggle}
+                        className="collapsible-container__button"
+                    >
+                        {this.state.active ? 'See Less': 'See More'}
+                    </button>
+                    <button 
+                        onClick={this.loadData}
+                        className="collapsible-container__button"
+                    >
+                        Load Data
+                    </button>
+                </div>
                 <div className="collapsible-container__wrapper" style={this.getWrapperStyle()}>
-                <div className="collapsible-container__element">Hidden Element 1</div>
-                <div className="collapsible-container__element">Hidden Element 2</div>
-                <div className="collapsible-container__element">Hidden Element 3</div>
-                <div className="collapsible-container__element">Hidden Element 4</div>
-                <div className="collapsible-container__element">Hidden Element 5</div>
+                    <div ref={this.tab}>
+                        {this.state.content}
+                    </div>
                 </div>
             </div>
       )
